279798ce30bb6e1683cdec8538afd6fa2b1f8139
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2023-01-28 10:05:04 +0700

    Updates On File

--- resources/views/map/location.blade.php
+++ resources/views/map/location.blade.php
@@ -19,9 +19,20 @@
 <form action="/map/location" method="post">
     @csrf
 
+    {{-- Errors --}}
     @if ($errors->has('location'))
         <div class="alert alert-danger" role="alert">
             {{ $errors->first('location') }}
+        </div>
+    @endif
+    @if ($errors->has('lat'))
+        <div class="alert alert-danger" role="alert">
+            {{ $errors->first('lat') }}
+        </div>
+    @endif
+    @if ($errors->has('long'))
+        <div class="alert alert-danger" role="alert">
+            {{ $errors->first('long') }}
         </div>
     @endif
 
@@ -46,43 +57,7 @@
 </form>
 
 @section('script')
-
-<script>
-// Long and Lat
-let long = $('#longitude');
-let lat = $('#latitude');
-
-// Controll Map View
-let map = L.map('map', {
-    center: [41.505, -0.09],
-    zoom: 15
-});
-
-// Make sure Map is Not Grey
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
-
-// Make Pop up Appear
-var popup = L.popup();
-
-// Popup Function
-onMapClick = (e) => {
-    popup
-        .setLatLng(e.latlng)
-        .setContent("Lokasi Terpilih, silakan berikan nama")
-        .openOn(map);
-
-    // Change Value
-    long.val(e.latlng.lng);
-    lat.val(e.latlng.lat);
-}
-
-// Listener
-map.on('click', onMapClick);
-</script>
-
+    <script src="{{ asset('JS/Map/location.js') }}"></script>
 @endsection
 
 @endsection
